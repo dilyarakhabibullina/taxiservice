951ace92e8d8116b4e7d786e61bb6dbee1b90548
dilyarakhabibullina <khdilyara@rambler.ru>
2019-09-24 23:32:12 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Taxiservice taxiservice = new Taxiservice();
+        System.out.println(taxiservice.calculateOrder());
+    }
+}
---
+++ src/main/java/Taxiservice.java
@@ -0,0 +1,24 @@
+public class Taxiservice {
+
+
+    public int calculateOrder() {
+        int deposit = 60;
+        int costPerKm = 20;
+        int distance = 100;
+        int discountMinOrder = 1000;
+        int maxDiscount = 100;
+        int discountPercent = 5;
+        int orderSum = deposit + (costPerKm * distance);
+        int discount = orderSum * discountPercent / 100;
+        if (discount > maxDiscount) {
+            discount = 100;
+        }
+
+        if (orderSum > discountMinOrder) {
+            int finalOrder = orderSum - discount;
+            return finalOrder;
+        } else {
+            return orderSum;
+        }
+    }
+}
