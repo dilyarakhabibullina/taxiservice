c25d474f91e856d3acceb96128a2077ef77916de
dilyarakhabibullina <khdilyara@rambler.ru>
2019-10-07 20:58:16 +0300

    fixes #5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
-public class Main {
-    public static void main(String[] args) {
-        Taxiservice taxiservice = new Taxiservice();
-        System.out.println(taxiservice.calculateOrder(100));
-    }
-}
+//public class Main {
+//    public static void main(String[] args) {
+//        Taxiservice taxiservice = new Taxiservice();
+//        System.out.println(taxiservice.calculateOrder(100));
+//    }
+//}
--- src/test/java/TaxiserviceTest.java
+++ src/test/java/TaxiserviceTest.java
@@ -1,9 +1,6 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 class TaxiserviceTest {
-
     @Test
     void calculateOrder() {
         Taxiservice service = new Taxiservice();
@@ -11,13 +8,15 @@ class TaxiserviceTest {
         assertEquals(160, orderSum);
     }
 
-    {
+    @Test
+    void calculateOrderWithDiscount() {
         Taxiservice service = new Taxiservice();
         int orderSum = service.calculateOrder(100);
         assertEquals(1960, orderSum);
     }
 
-    {
+    @Test
+    void calculateAnotherOrderWithDiscount() {
         Taxiservice service = new Taxiservice();
         int orderSum = service.calculateOrder(90);
         assertEquals(1767, orderSum);
